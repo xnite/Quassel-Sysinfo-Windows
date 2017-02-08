d27c3d1cfe1a8c9da2899f7e025aab27bea8c2ea
Robert Whitney <xnite@xnite.me>
2017-02-08 13:19:55 -0600

    initial commit

---
+++ SysInfo/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace SysInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Measure CPU */
+            PerformanceCounter cpuCounter;
+            cpuCounter = new PerformanceCounter();
+            cpuCounter.CategoryName = "Processor";
+            cpuCounter.CounterName = "% Processor Time";
+            cpuCounter.InstanceName = "_Total";
+            cpuCounter.NextValue();
+            System.Threading.Thread.Sleep(500);
+
+            /* Measure Memory */
+            Int64 phav = PerformanceInfo.GetPhysicalAvailableMemoryInMiB();
+            Int64 tot = PerformanceInfo.GetTotalMemoryInMiB();
+            decimal percentFree = ((decimal)phav / (decimal)tot) * 100;
+            decimal percentOccupied = 100 - percentFree;
+
+            /*
+            // Process List
+            // I WILL USE THIS LATER FOR PROCESS COUNTING - xnite
+            // Start the child process.
+            Process p = new Process();
+            // Redirect the output stream of the child process.
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.FileName = "tasklist.exe";
+            p.Start();
+            // Do not wait for the child process to exit before
+            // reading to the end of its redirected stream.
+            // p.WaitForExit();
+            // Read the output stream first and then wait.
+            string processes = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            */
+            /* Write results out to console */
+            Console.WriteLine("[SysInfo for " + Environment.MachineName + "] [OS: " + Environment.OSVersion.ToString() + "][Memory:: " + tot + "MB " + Math.Round(percentFree, 2) + "% free] [CPU: " + System.Environment.ProcessorCount + "x " + System.Environment.GetEnvironmentVariable("PROCESSOR_IDENTIFIER").ToString() + " CPU Usage:" + Math.Round(cpuCounter.NextValue(), 2) + "%]");
+        }
+    }
+    public static class PerformanceInfo
+    {
+
+        [DllImport("psapi.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetPerformanceInfo([Out] out PerformanceInformation PerformanceInformation, [In] int Size);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PerformanceInformation
+        {
+            public int Size;
+            public IntPtr CommitTotal;
+            public IntPtr CommitLimit;
+            public IntPtr CommitPeak;
+            public IntPtr PhysicalTotal;
+            public IntPtr PhysicalAvailable;
+            public IntPtr SystemCache;
+            public IntPtr KernelTotal;
+            public IntPtr KernelPaged;
+            public IntPtr KernelNonPaged;
+            public IntPtr PageSize;
+            public int HandlesCount;
+            public int ProcessCount;
+            public int ThreadCount;
+        }
+
+        public static Int64 GetPhysicalAvailableMemoryInMiB()
+        {
+            PerformanceInformation pi = new PerformanceInformation();
+            if (GetPerformanceInfo(out pi, Marshal.SizeOf(pi)))
+            {
+                return Convert.ToInt64((pi.PhysicalAvailable.ToInt64() * pi.PageSize.ToInt64() / 1048576));
+            }
+            else
+            {
+                return -1;
+            }
+
+        }
+
+        public static Int64 GetTotalMemoryInMiB()
+        {
+            PerformanceInformation pi = new PerformanceInformation();
+            if (GetPerformanceInfo(out pi, Marshal.SizeOf(pi)))
+            {
+                return Convert.ToInt64((pi.PhysicalTotal.ToInt64() * pi.PageSize.ToInt64() / 1048576));
+            }
+            else
+            {
+                return -1;
+            }
+
+        }
+    }
+}
